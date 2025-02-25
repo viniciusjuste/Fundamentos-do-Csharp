65945591fcde3ffed97e80059b132574579712e5
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-02-25 20:32:51 -0300

    Struct

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -2,19 +2,30 @@
 {
     private static void Main(string[] args)
     {
-        var arr = new string[2];
-        arr[0] = "Item 1";
+        var product = new Product(1, "TV", 500.00);
+        product.Price = product.PriceInDolar(5.0);
 
-        var arr2 = arr;
-
-
-        Console.WriteLine(arr[0]);
-        Console.WriteLine(arr2[0]);
-
-        arr[0] = "Item 2";
-
-        Console.WriteLine(arr[0]);
-        Console.WriteLine(arr2[0]);
+        Console.WriteLine(product.Id);
+        Console.WriteLine(product.Name);
+        Console.WriteLine(product.Price);
     }
-
 }
+
+struct Product
+{
+    public Product(int id, string name, double price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+    public int Id;
+    public string Name;
+    public double Price;
+
+    public double PriceInDolar(double dolar)
+    {
+        return Price * dolar;
+    }
+}
+
