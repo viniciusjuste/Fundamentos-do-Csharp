fbea134b5641e15ea7e7cbc73440c00e4207ac56
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-02-24 21:17:37 -0300

    Operadores aritméticos

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -2,19 +2,24 @@
 {
     private static void Main(string[] args)
     {
-        int inteiro = 100;
-        Console.WriteLine(inteiro);
+        // int inteiro = 100;
+        // Console.WriteLine(inteiro);
 
-        float real = 25.5f;
-        Console.WriteLine(real);
+        // float real = 25.5f;
+        // Console.WriteLine(real);
 
-        // Conversão implicita
-        //  real = inteiro;
-        //  Console.WriteLine(real);
+        // // Conversão implicita
+        // //  real = inteiro;
+        // //  Console.WriteLine(real);
 
-        //  inteiro = (int)real;
+        // //  inteiro = (int)real;
 
-        inteiro = Convert.ToInt32(real);
-        Console.WriteLine(inteiro);
+        // inteiro = Convert.ToInt32(real);
+        // Console.WriteLine(inteiro);
+
+        float nota1 = 5.5f;
+        float nota2 = 6.5f;
+        float media = (nota1 + nota2) / 2;
+        Console.WriteLine($"Meedia: {media}");
     }
 }
