259d5828013c31a8e5c0e15fdae02dbf6e86de45
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-02-26 21:04:59 -0300

    Enums

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -1,27 +1,32 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        var product = new Product(1, "TV", 500.00);
+        var product = new Product(1, "TV", 500.00, EproductType.Product);
         product.Price = product.PriceInDolar(5.0);
 
         Console.WriteLine(product.Id);
         Console.WriteLine(product.Name);
         Console.WriteLine(product.Price);
+        Console.WriteLine((int)product.Type);
     }
 }
 
 struct Product
 {
-    public Product(int id, string name, double price)
+    public Product(int id, string name, double price, EproductType type)
     {
         Id = id;
         Name = name;
         Price = price;
+        Type = type;
     }
     public int Id;
     public string Name;
     public double Price;
+    public EproductType Type;
 
     public double PriceInDolar(double dolar)
     {
@@ -29,3 +34,8 @@ struct Product
     }
 }
 
+enum EproductType
+{
+    Product = 1,
+    Service = 2
+}
--- Teste/obj/Debug/net9.0/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net9.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+914091144c38eb1beea898269ac946b90cf574bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65945591fcde3ffed97e80059b132574579712e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
