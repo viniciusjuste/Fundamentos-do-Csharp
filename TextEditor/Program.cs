a2bc81c1ae71efcebb2fa054a9d7ed34bfe64b71
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-11 18:34:49 -0300

    Criando um editor de arquivos

---
+++ TextEditor/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("o quê você deseja fazer ?");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: OpenFile(); break;
+                case 2: EditFile(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void OpenFile()
+        {
+
+        }
+
+        static void EditFile()
+        {
+
+        }
+    }
+}
