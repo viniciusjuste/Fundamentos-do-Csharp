9a0b1fb46aae4120fd7085044b96d1d20db95c45
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-12 18:52:54 -0300

    Usando Guid

---
+++ MyApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Name
+{
+    class Program {
+        static void Main(string[] args){
+            var id = Guid.NewGuid();
+            id.ToString().Substring(0, 8);
+            Console.WriteLine(id);
+        }
+    }
+}
