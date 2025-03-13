50eb120c58320c6e6af37564c58c8c55a68ff627
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-13 19:59:17 -0300

    Projeto editor HTML

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,11 @@
+public static class Menu{
+    public static void Show(){
+    Console.Clear();
+    Console.WriteLine("O que voce deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine("----------------");
+    Console.WriteLine("Selecione uma opção: ");
+}
+}
