1d6a22e8a4557a8524845dc0cfd644e30302efdc
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-17 19:26:06 -0300

    Iniciando com datas

---
+++ LearnAboutDates/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+      DateTime date = DateTime.Now;
+      Console.WriteLine(date);
+
+     var day = date.DayOfWeek;
+     Console.WriteLine(day);
+
+     var year = date.Year;
+     Console.WriteLine(year);
+
+     var month = date.Month;
+     Console.WriteLine(month);
+    }
+}
