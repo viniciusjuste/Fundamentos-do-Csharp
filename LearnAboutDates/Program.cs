5237747fa2cbe5bb25f1872e58ac00f48785f75d
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-19 17:09:00 -0300

    Padrões de formatação

--- LearnAboutDates/Program.cs
+++ LearnAboutDates/Program.cs
@@ -3,7 +3,11 @@
     static void Main(string[] args)
     {
         var date = DateTime.Now;
-        var formato = string.Format("{0: dd/MM/yyyy}", date);
+        // var formato = string.Format("{0:s}", date);
+        // var formato = string.Format("{0:r}", date);
+        var formato = string.Format("{0:u}", date);
+
+        Console.Clear();
         Console.WriteLine(formato);
     }
 }
