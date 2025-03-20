426ad824b6ff646f8759ab47a4d6684d5aa130a7
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-20 20:20:31 -0300

    math

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -5,9 +5,8 @@ class Program
     static void Main(string[] args)
     {
         decimal valor = 10.25m;
-        var culture = CultureInfo.CreateSpecificCulture("pt-BR");
 
         Console.Clear();
-        Console.WriteLine(valor.ToString("C",culture));
+        Console.WriteLine(Math.Floor(valor));
     }
 }
--- Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
+++ Numbers/obj/Debug/net9.0/Numbers.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+694ac1cc22f666fb9a2647689b1ded1ea64da601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1c96e848509946972f3e00ba033116a0cd8eb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numbers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
