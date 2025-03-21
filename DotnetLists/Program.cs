c8ee6e2faad5198ae7d57f20cf5084b5a4e4018d
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-03-21 18:37:01 -0300

    Arrays

---
+++ DotnetLists/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+      var meuArray = new int[5];
+      int[] array = new int[5];
+
+      meuArray[0] = 1;
+
+      Console.WriteLine(meuArray[0]);
+    }
+}
